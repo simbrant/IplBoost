34fcbf8b4af86432c06522cffd049c674f8e01b3
simbrant <simbrant@hotmail.com>
2018-03-14 15:29:31 +0100

    Corrected function

--- R/IplBoost.R
+++ R/IplBoost.R
@@ -56,7 +56,10 @@ IplBoost <- function(times, status, mat, lms, w, M, lambda, verbose=FALSE, stand
   if (standardise) {
     for (m in 2:M){
       estimates[[m]] <- as.matrix(estimates[[m]])
-      .scale_columns(estimates[[m]], sds, dim(estimates[[m]])[1], dim(estimates[[m]])[2])
+      #.scale_columns(estimates[[m]], sds, dim(estimates[[m]])[1], dim(estimates[[m]])[2])
+      for (j in 1:length(sds)){
+        estimates[[m]][, j] <-  estimates[[m]][, j]/sds[j]
+      }
     }
   }
   
